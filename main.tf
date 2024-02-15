dc7d107ed82ac57c212392ef85b9ec1c951a0b1b
Grant Orchard <go@hashicorp.com>
2024-02-16 09:19:50 +1100

    bump module version for testing

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "postgres" {
 # --- Create Boundary targets for the postgres nodes
 module "boundary_target" {
   source  = "app.terraform.io/tfo-apj-demos/target/boundary"
-  version = "1.0.1-alpha"
+  version = "1.0.2-alpha"
 
   hosts = [
     { 
