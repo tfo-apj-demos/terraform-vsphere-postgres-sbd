0ad54108a9f46a6fae8fad54eff0afc639db9cf9
Grant Orchard <go@hashicorp.com>
2024-02-16 12:37:33 +1100

    pass through vault ca

--- main.tf
+++ main.tf
@@ -70,9 +70,9 @@ module "boundary_target" {
   host_catalog_id = "hcst_7B2FWBRqb0"
   hostname_prefix = "postgres_sbd"
   credential_store_token = module.database_secrets.token
-  vault_address = "https://vault.gcve.local:8200"
+  vault_address = "https://vault.hashicorp.local:8200"
   #injected_credential_library_ids = ["clvsclt_bDETPnhh75"]
-  vault_ca_cert = file("${path.root}/ca_cert_dir/ca_chain.pem")
+  #vault_ca_cert = file("${path.root}/ca_cert_dir/ca_chain.pem")
   
 }
 
